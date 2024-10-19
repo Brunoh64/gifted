efe10ecee50bf0369c9f4066281cedec6c4b1fb9
Brunoh64 <brunohmutua48@gmail.com>
2024-10-19 22:33:54 +0300

    Update set.cjs

--- set.cjs
+++ set.cjs
@@ -27,8 +27,8 @@ const config = {
   MODE: process.env.MODE || 'public',
   PREFIX: process.env.PREFIX || '.',
   PORT: process.env.PORT || "5000", /* Where App Will Listen On */
-  OWNER_NAME: process.env.OWNER_NAME || "ɢɪғᴛᴇᴅ ᴛᴇᴄʜ",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "254762016957",
+  OWNER_NAME: process.env.OWNER_NAME || "brunoh🫠",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "254111270158",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false,
 };
 
